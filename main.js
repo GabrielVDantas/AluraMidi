7a4ad1f572dc61bff14bcbbffc79328d0fb3443f
Gabriel Viegas  Dantas <gvdantas2028@gmail.com>
2023-10-01 14:32:10 -0300

    Adicionando comentários ao código JavaScript

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// A função tocarSom(id) em JavaScript é projetada para reproduzir um som 
+// de um elemento de áudio identificado pelo id fornecido como argumento
 function tocarSom (id) {
     const elemento = document.querySelector(id);
 
@@ -11,27 +13,21 @@ function tocarSom (id) {
 }
 
 const todosOsBotoes = document.querySelectorAll('.botao');
-
+// eventos de clique para botões com base nas suas classes para criar uma interface de reprodução de som interativa
 for (let i = 0; i < todosOsBotoes.length; i++) {
-
     const botao = todosOsBotoes[i];
     const som = botao.classList[1];
-    const idSom = `#som_${som}`; //template string
+    const idSom = `#som_${som}`;
 
     botao.onclick = function () {
         tocarSom(idSom);
     }
-
     botao.onkeydown = function (evento) {
-
         if (evento.code === 'Space' || evento.code === 'Enter') {
             botao.classList.add('ativa');
         }
-
     }
-
     botao.onkeyup = function () {
         botao.classList.remove('ativa');
     }
-
 }
